1edf9039cfbfff14589ba7f39b7d3b771ac73fa3
Dtrip <d@nrx.co>
2015-10-18 21:06:27 -0700

    initial files

---
+++ rc.lua
@@ -0,0 +1,55 @@
+-- Standard awesome library
+gears = require("gears")
+awful = require("awful")
+awful.rules = require("awful.rules")
+require("awful.autofocus")
+-- Widget and layout library
+wibox = require("wibox")
+-- Theme handling library
+beautiful = require("beautiful")
+-- Notification library
+naughty = require("naughty")
+menubar = require("menubar")
+
+-- {{{ Error handling
+require("errors")
+
+-- {{{ Variable definitions
+-- Themes define colours, icons, font and wallpapers.
+-- beautiful.init("@AWESOME_THEMES_PATH@/default/theme.lua")
+beautiful.init("~/.config/awesome/themes/powerarrows/theme.lua")
+
+-- This is used later as the default terminal and editor to run.
+terminal = "terminator"
+editor = os.getenv("EDITOR") or "vim"
+editor_cmd = terminal .. " -e " .. editor
+
+-- Default modkey.
+-- Usually, Mod4 is the key with a logo between Control and Alt.
+-- If you do not like this or do not have such a key,
+-- I suggest you to remap Mod4 to another key using xmodmap or other tools.
+-- However, you can use another modifier like Mod1, but it may interact with others.
+modkey = "Mod4"
+
+
+require("layouts")
+
+-- {{{ Helper functions
+require("helpers")
+
+-- {{{ Wallpaper
+require("wallpaper")
+
+-- {{{ Tags
+require("tags")
+
+-- {{{ Menu
+require("menubars")
+
+require("bindings")
+
+-- {{{ Rules
+require("rules")
+
+
+require("signals")
---
+++ wallpaper.lua
@@ -0,0 +1,7 @@
+
+if beautiful.wallpaper then
+    gears.wallpaper.maximized(beautiful.wallpaper)
+    -- for s = 1, screen.count() do
+    --     gears.wallpaper.maximized(beautiful.wallpaper, s, true)
+    -- end
+end
