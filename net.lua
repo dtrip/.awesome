7214e39772c56f9dd0f26609667e5c3a3f85ed83
Dtrip <d@nrx.co>
2016-01-19 10:12:52 -0800

    updates to blingbling

--- net.lua
+++ net.lua
@@ -8,7 +8,7 @@ wifiwidgetSSID:set_widget(wifiwidget_txt)
 wifiwidgetSSID:set_bg(beautiful.arrow_bg_2)
 wifiwidgetSSID:set_fg(beautiful.datewidget_fg)
 -- -- wifiwidget:set_border_color(beautiful.arrow_bg_2)
-vicious.register(wifiwidget_txt, vicious.widgets.wifi, " ${ssid} ", 3, 'mlan0')
+vicious.register(wifiwidget_txt, vicious.widgets.wifi, " ${ssid} ", 3, 'wlp1s0')
 --
 --
 local netCfg = {
@@ -23,5 +23,6 @@ local netCfg = {
     graph_color = beautiful.bg_normal
 }
 wifiwidget = blingbling.triangular_progress_graph(netCfg)
-vicious.register(wifiwidget, vicious.widgets.wifi, "${linp}", 3, 'mlan0')
+vicious.register(wifiwidget, vicious.widgets.wifi, "${linp}", 3, 'wlp1s0')
+-- vicious.register(wifiwidget, vicious.widgets.wifi, "${linp}", 3, 'mlan0')
 
