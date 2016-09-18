b954e1963d663d35ba8f4f24960ea4cc459742b8
dtrip <dtrip@hp.nrx.co>
2016-09-17 21:19:07 -0700

    updates

--- rc.lua
+++ rc.lua
@@ -28,7 +28,7 @@ awful.util.spawn_with_shell("bash " .. CONFIG_PATH .. "/scripts/nm-applet.bash &
 -- used to start solaar applet for logitech keyboards
 awful.util.spawn_with_shell("bash " .. CONFIG_PATH .. "/scripts/solaar.bash &")
 awful.util.spawn_with_shell("bash " .. CONFIG_PATH .. "/scripts/indicator-sound.bash")
-awful.util.spawn_with_shell("bash " .. CONFIG_PATH .. "/scripts/blueman-applet.bash")
+-- awful.util.spawn_with_shell("bash " .. CONFIG_PATH .. "/scripts/blueman-applet.bash")
 
 -- starts conky widgets
 -- awful.util.spawn_with_shell("sh ~/conky/start-conky.sh &")
