554b2ad6f219cc5ce9b0abcaa149bd08efac0d1f
dtrip <d@nrx.co>
2017-01-13 09:01:57 -0800

    updates

--- rc.lua
+++ rc.lua
@@ -20,17 +20,17 @@ CONFIG_PATH = os.getenv("HOME") .. "/.config/awesome"
 
 -- {{{ startup scripts
 -- awful.util.spawn_with_shell("bash " .. CONFIG_PATH .. "/scripts/xcomp.bash &")
--- awful.util.spawn_with_shell("bash " .. CONFIG_PATH .. "/scripts/compton.bash &")
+awful.util.spawn_with_shell("bash " .. CONFIG_PATH .. "/scripts/compton.bash &")
 awful.util.spawn_with_shell("xmodmap " .. CONFIG_PATH .. "/scripts/.Xmodmap &")
 
 -- sets network monitor applet in taskbar
 awful.util.spawn_with_shell("bash " .. CONFIG_PATH .. "/scripts/nm-applet.bash &")
 
 -- used to start solaar applet for logitech keyboards
--- awful.util.spawn_with_shell("bash " .. CONFIG_PATH .. "/scripts/solaar.bash &")
--- awful.util.spawn_with_shell("bash " .. CONFIG_PATH .. "/scripts/indicator-sound.bash")
+awful.util.spawn_with_shell("bash " .. CONFIG_PATH .. "/scripts/solaar.bash &")
+awful.util.spawn_with_shell("bash " .. CONFIG_PATH .. "/scripts/indicator-sound.bash")
 awful.util.spawn_with_shell("bash " .. CONFIG_PATH .. "/scripts/pulseaudio.bash")
--- awful.util.spawn_with_shell("bash " .. CONFIG_PATH .. "/scripts/blueman-applet.bash")
+awful.util.spawn_with_shell("bash " .. CONFIG_PATH .. "/scripts/blueman-applet.bash")
 
 -- starts conky widgets
 -- awful.util.spawn_with_shell("sh ~/conky/start-conky.sh &")
@@ -140,10 +140,10 @@ myawesomemenu = {
 
 mymainmenu = awful.menu({ items = {
                                     { "Firefox", "firefox", beautiful.firefox },
-                                    -- { "Chrome", "chromium-browser", beautiful.www_icon },
-                                    { "Chrome", "chromium", beautiful.www_icon },
-                                    -- { "Incognito Chrome", "chromium-browser --incognito", beautiful.www_icon },
-                                    { "Incognito Chrome", "chromium --incognito", beautiful.www_icon },
+                                    { "Chrome", "chromium-browser", beautiful.www_icon },
+                                    -- { "Chrome", "chromium", beautiful.www_icon },
+                                    { "Incognito Chrome", "chromium-browser --incognito", beautiful.www_icon },
+                                    -- { "Incognito Chrome", "chromium --incognito", beautiful.www_icon },
                                     { "Burp Suite", "burpsuite", beautiful.burp_icon },
                                     { "Terminal", terminal, beautiful.term_icon },
                                     { "SQLiteStudio", "sqlitestudio", beautiful.sqlite_icon },
